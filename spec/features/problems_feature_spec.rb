776bdb3f2e887d1fa95c2527e7e88b6e69101f6e
michaellennox <michaelctlennox@gmail.com>
2016-03-16 13:38:23 +0000

    Adds failing feature test for viewing problems at /problems

--- spec/features/problems_feature_spec.rb
+++ spec/features/problems_feature_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
-feature 'Problems' do
-  scenario 'should display a problems header' do
-    visit '/'
-    expect(page).to have_content('Problems')
+feature 'Problems Features.' do
+  context 'Given the database contains some problems.' do
+    before :each do
+      FactoryGirl.create(:problem, title: 'A Big Big Problem')
+      FactoryGirl.create(:problem, title: 'A Teeny Tiny Problem')
+    end
+
+    context 'When a user has visited the problems page.' do
+      before :each do
+        visit '/problems'
+      end
+
+      scenario 'The user should see a list of problem titles' do
+        within 'ul#problems' do
+          expect(page).to have_content 'A Big Big Problem'
+          expect(page).to have_content 'A Teeny Tiny Problem'
+        end
+      end
+    end
   end
 end
